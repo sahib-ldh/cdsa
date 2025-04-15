56628cd9c740346b3aecb61febab49c962262260
sahib-ldh <itiglehunt@gmail.com>
2025-04-15 23:39:18 +0530

    anoter practise question

--- AndOr/andor3.c
+++ AndOr/andor3.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 
 int main() {
- 
-    int a = 1,b=2,c=1,d;
-    d= a-- && ++b || --c && printf("sahib");
-    printf("%d %d %d %d", a,b,c,d);
-    //OUTPUT is 0 3 1 1 and short circuit so sahib will not print 
+    int a = 4, b = 5, c = 3, d = 2, e = 7, f = 8;
+    int result;
+
+    // A complex arithmetic expression with precedence and short-circuit evaluation
+    result = (a++ * b-- + c++ - d++ || e-- + f-- % a) && (a * b - ++c + d % 2);
+
+    printf("a = %d, b = %d, c = %d, d = %d, e = %d, f = %d\n", a, b, c, d, e, f);
+    printf("result = %d\n", result);
+
     return 0;
 }
-
